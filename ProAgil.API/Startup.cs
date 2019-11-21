38aa88a1eeb6f202f35abdb4fdfac388580ed17b
Thales Silva <thales.silva@iatec.com>
2019-11-21 14:01:18 -0300

    criando component

--- ProAgil.API/Startup.cs
+++ ProAgil.API/Startup.cs
@@ -30,7 +30,7 @@ namespace ProAgil.API
             services.AddDbContext<DataContext>(
                 x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
             );
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
